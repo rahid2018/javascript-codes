8713797e64bce41c3fdbb1869325269441472f9b
rahid2018 <157884204+rahid2018@users.noreply.github.com>
2025-09-20 13:56:00 +0000

    conversion

---
+++ comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 1)//true js converts string to number automatically
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // true treats null as 0 and equality check and comparisons work differentle
+
+console.log(undefined == 0) // >,>=,<=, < all return false
+
+// === checks both datatype and values
+console.log("2"===2)
