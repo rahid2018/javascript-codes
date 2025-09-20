2a8040837a0fb396da4a4ac6f75555c3acf8d498
rahid2018 <157884204+rahid2018@users.noreply.github.com>
2025-09-20 13:46:23 +0000

    conversion prefix postfix

--- conversionOperation.js
+++ conversionOperation.js
@@ -21,4 +21,33 @@ let BooleanIsLoggedin = Boolean(isLoggedin)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ===== operations =========
+
+let value = 3
+let negValue = -value
+// console.log(negValue)//-3
+
+let str1 = "hello"
+let str2 = " Rahid"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+3); //13
+// console.log(1+"3"); //13
+// console.log("1"+3+2); //132 if string first then all treated as string
+// console.log(1+3+"2"); //42 if string last then first conversion happens
+
+// console.log(true)// true
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // not good way for readability
+
+let gameCounter = 100
+++gameCounter// prefix increment, value gets incremented at begining of usage
+gameCounter++ // value gets incremented after usage
+console.log(gameCounter)
+
+
