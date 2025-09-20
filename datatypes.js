ec06a4a80e4babdc7b5ee16e3108d22c5172aca5
rahid2018 <157884204+rahid2018@users.noreply.github.com>
2025-09-20 13:09:02 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treats all js code as newer version
+// alert(3+3)//will throw an error, because we are using nodejs not browser
+console.log(3 + 4)
+
+let name ="Rahid"
+let age = 23
+let isLoggedIn = false
+let state = null // means that it is not undefined but is empty/null
+
+
+// ====premitive========
+
+// number => 2 to power 53
+// "bigint" => used for large numbers like stock market
+// string => double / single quote
+// boolean => true/false
+// null => standalone value
+// undefined => declared but value not assigned
+// Symbol => to find uniqueness
+
+
+//  ========Object======
+
+console.log(typeof age) // string
+console.log(typeof null)// object (language mistake)
+console.log(typeof undefined)// undefined
